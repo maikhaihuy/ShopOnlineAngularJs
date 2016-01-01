7d8c93937dd0a309e62792337b2015450680d545
Mai Huy <mhuy1602@gmail.com>
2016-01-01 12:51:21 +0700

    test

--- src/app.js
+++ src/app.js
@@ -93,18 +93,3 @@ shopOnl.run(function ($http, AuthenticationService) {
     $http.defaults.headers.common.Authorization = 'Basic NTY0ODA0OGY2MmJkOTYxMTAwODc4NTI1OnZlcnkgc2VjdXJl==';
     $http.defaults.headers.post['Content-Type'] = 'application/json';
 });
-
-shopOnl.directive('pwCheck', [function () {
-    return {
-      require: 'ngModel',
-      link: function (scope, elem, attrs, ctrl) {
-        var firstPassword = '#' + attrs.pwCheck;
-        elem.add(firstPassword).on('keyup', function () {
-          scope.$apply(function () {
-            var v = elem.val()===$(firstPassword).val();
-            ctrl.$setValidity('pwmatch', v);
-          });
-        });
-      }
-    }
-  }]);
